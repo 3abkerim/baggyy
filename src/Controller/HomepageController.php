c3ebcecf9e724c7c14f86f32b763f25297d93515
Abdelkerim Abdelaziz <122649417+3abkerim@users.noreply.github.com>
2025-02-20 10:32:10 +0100

    fix: homepage controller route

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomepageController extends AbstractController
 {
-    #[Route('/homepage', name: 'app_homepage')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig', [
