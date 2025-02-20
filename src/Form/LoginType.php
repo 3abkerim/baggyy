bf71b4b961e8c4fd0d5a75b9347fd7363d208a1e
Abdelkerim Abdelaziz <122649417+3abkerim@users.noreply.github.com>
2025-02-20 10:31:00 +0100

    feat: login $ registration type and controller

---
+++ src/Form/LoginType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Email address', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [],
+                'required' => true,
+                'trim' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+                'required' => true,
+                'attr' => [],
+            ])
+            ->add('remember_me', CheckboxType::class, [])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Login',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [],
+                'required' => true,
+                'trim' => true,
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password'],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Create Account',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
