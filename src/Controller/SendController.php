831d172fa424dc479b2067f51b9f6d7ad9970a0a
Abdelkerim Abdelaziz <122649417+3abkerim@users.noreply.github.com>
2025-04-06 13:52:04 +0200

    feat: add send and shop controllers

---
+++ src/Controller/SendController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SendController extends AbstractController
+{
+    #[Route('/send', name: 'send')]
+    public function index(): Response
+    {
+        return $this->render('send/index.html.twig', [
+            'controller_name' => 'SendController',
+        ]);
+    }
+}
